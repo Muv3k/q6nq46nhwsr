cac679e75e2227478b867651b8f61d89cb49d7af
Muv3k <Muv3k@github.com>
2020-08-18 04:33:13 +0300

    Updates

--- precache-manifest.b5924c2e3ef86a4274427c4cd8cf670f.js
+++ precache-manifest.b5924c2e3ef86a4274427c4cd8cf670f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83f114c2da3f6065bb33a7e24253be29",
+    "revision": "00c64aff1d495a5b7db0e3420426ddab",
     "url": "/q6nq46nhwsr/index.html"
   },
   {
-    "revision": "eeca58aa16338fbe52d6",
+    "revision": "bfeef464aded8fd43945",
     "url": "/q6nq46nhwsr/static/css/2.1e562f28.chunk.css"
   },
   {
-    "revision": "2a0f080ed7e71f8eee95",
+    "revision": "1b6a8808a84b81a66037",
     "url": "/q6nq46nhwsr/static/css/main.daf6e6d3.chunk.css"
   },
   {
-    "revision": "eeca58aa16338fbe52d6",
-    "url": "/q6nq46nhwsr/static/js/2.2fef53fe.chunk.js"
+    "revision": "bfeef464aded8fd43945",
+    "url": "/q6nq46nhwsr/static/js/2.78eed4a4.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "/q6nq46nhwsr/static/js/2.2fef53fe.chunk.js.LICENSE.txt"
+    "url": "/q6nq46nhwsr/static/js/2.78eed4a4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2a0f080ed7e71f8eee95",
-    "url": "/q6nq46nhwsr/static/js/main.7128a2ad.chunk.js"
+    "revision": "1b6a8808a84b81a66037",
+    "url": "/q6nq46nhwsr/static/js/main.022365cf.chunk.js"
   },
   {
     "revision": "223859405522b625d061",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/q6nq46nhwsr/precache-manifest.caa2ddb5886b8f29be4524c5c2173e8d.js"
+  "/q6nq46nhwsr/precache-manifest.b5924c2e3ef86a4274427c4cd8cf670f.js"
 );
 
 self.addEventListener('message', (event) => {
